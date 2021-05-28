b5ee7b3576b6383b1ec679a95bfd02a325a48ba2
Malh1n <58389533+Malh1n@users.noreply.github.com>
2021-05-28 19:29:00 +0300

    28.05.2021

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -8,36 +8,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    private final static String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS users " +
-            "(Id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20), lastName VARCHAR(20), age INT)";
-    private final static String DROP_TABLE = "DROP TABLE IF EXISTS users";
-    private final static String DELETE_BY_ID = "DELETE FROM users WHERE Id = ?";
-    private final static String CLEAR_ALL_USERS = "TRUNCATE users;";
-    private final static String INSERT_USER = "INSERT INTO users(name, lastName, age) VALUES (?, ?, ?)";
-    private final static String GET_ALL_USERS = "SELECT * FROM users";
 
     public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
-        try (Statement statement = Util.getMySQLConnection().createStatement()) {
-            statement.executeUpdate(CREATE_TABLE);
+        try (Connection connection = Util.getMySQLConnection(); Statement statement = connection.createStatement()) {
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS users" +
+                    " (Id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20), lastName VARCHAR(20), age INT)");
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void dropUsersTable() {
-        try (Statement statement = Util.getMySQLConnection().createStatement()) {
-            statement.executeUpdate(DROP_TABLE);
+        try (Connection connection = Util.getMySQLConnection(); Statement statement = connection.createStatement()) {
+            statement.executeUpdate("DROP TABLE IF EXISTS users");
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (Connection connection = Util.getMySQLConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USER)) {
+        try (Connection connection = Util.getMySQLConnection(); PreparedStatement preparedStatement = connection
+                .prepareStatement("INSERT INTO users(name, lastName, age) VALUES (?, ?, ?)")) {
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setString(3, String.valueOf(age));
@@ -49,8 +43,8 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void removeUserById(long id) {
-        try (Connection connection = Util.getMySQLConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(DELETE_BY_ID)) {
+        try (Connection connection = Util.getMySQLConnection(); PreparedStatement preparedStatement = connection
+                .prepareStatement("DELETE FROM users WHERE Id = ?")) {
             preparedStatement.setString(1, String.valueOf(id));
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
@@ -60,8 +54,8 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public List<User> getAllUsers() {
         List<User> list = new ArrayList<>();
-        try (Statement statement = Util.getMySQLConnection().createStatement();
-             ResultSet resultSet = statement.executeQuery(GET_ALL_USERS)) {
+        try (Connection connection = Util.getMySQLConnection(); Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM users")) {
             while (resultSet.next()) {
                 String name = resultSet.getString(2);
                 String lastName = resultSet.getString(3);
@@ -75,8 +69,8 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        try (Statement statement = Util.getMySQLConnection().createStatement()) {
-            statement.executeUpdate(CLEAR_ALL_USERS);
+        try (Connection connection = Util.getMySQLConnection(); Statement statement = connection.createStatement()) {
+            statement.executeUpdate("TRUNCATE users;");
         } catch (SQLException e) {
             e.printStackTrace();
         }
